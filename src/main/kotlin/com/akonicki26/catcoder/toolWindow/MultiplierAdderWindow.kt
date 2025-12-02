0db1c4c3718e3b50a722c17bfcf4627bbf2daa7f
Akonicki26 <anne.konicki@mymail.champlain.edu>
2025-12-01 19:21:03 -0500

    Fix bug where default multiplier is x0

--- src/main/kotlin/com/akonicki26/catcoder/toolWindow/MultiplierAdderWindow.kt
+++ src/main/kotlin/com/akonicki26/catcoder/toolWindow/MultiplierAdderWindow.kt
@@ -8,9 +8,10 @@ import javax.swing.JComboBox
 
 class MultiplierAdderWindow : ToolWindow {
 
-    var upgrade: Int = 0
+    private val options = arrayOf(2, 3, 4, 5, 6);
+    private var upgrade: Int = options[0]
     override fun getContent() = JBPanel<JBPanel<*>>().apply {
-        val options = arrayOf(2, 3, 4, 5, 6);
+
         val dropdown = JComboBox(options)
 
         add(AddMultiplerMessage.label)
